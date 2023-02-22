4372b4a020d09e2a16e6475c2e944002d44b3733
arun <nuranisafirdausy1201@gmail.com>
2023-02-22 08:54:58 +0700

    first commit

---
+++ Modul 1/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name"          : "New Addons",
+    "version"       : "1.0",
+    "Author"        : "Nur Anisa Firdausy",
+    "website"       : "https://www.detik.com",
+    "category"      : "New Modul",
+    "summary"       : "Ini adalah addons baru",
+    "description"   : """Latihan Membuat addons baru di odoo 1""",
+    "depends"       : ["base"],
+    "data"          : [],
+    "demo"          : [],
+    "test"          : [],
+    "images"        : [],
+    "qweb"          : [],
+    "css"           : [],
+    "application"   : True,
+    "installable"   : True,
+    "auto_install"  : False,
+    
+}
